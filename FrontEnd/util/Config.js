cdfb914d3966d2a1965ef05905c8c66f951e6426
RodrigoSchiavone <rodrigoschiavonero@gmail.com>
2023-11-18 17:29:50 -0300

    alteração do README e alteação dos containers.

--- FrontEnd/util/Config.js
+++ FrontEnd/util/Config.js
@@ -1,8 +1,9 @@
 const Config = {
-  API_URL: "http://api:3000/",
+  API_URL: "http://192.168.18.183:3000/",
   TIMEOUT_REQUEST: 5000,
   HEADER_REQUEST: {
     Accept: "application/json",
   },
 };
+
 export default Config;
