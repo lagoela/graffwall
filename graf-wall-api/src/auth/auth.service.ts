c992660f61ac6fa20628914cce6be9dca685bcda
RodrigoSchiavone <rodrigoschiavonero@gmail.com>
2023-11-19 15:13:04 -0300

    Implmentando o refreshToken para revalidar o usuario enquanto utiliza o app

--- graf-wall-api/src/auth/auth.service.ts
+++ graf-wall-api/src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private jwtService: JwtService,
+    private tokenService: TokenService,
   ) {}
 
   async signIn(email: string, pass: string): Promise<any> {
@@ -18,9 +20,21 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    const payload = { sub: user.email, username: user.senha };
+    const payload = { sub: user.email, username: user.id };
+    const NEWtoken = await this.jwtService.signAsync(payload);
+    this.tokenService.save(NEWtoken, user.email);
+
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: NEWtoken,
+    };
+  }
+
+  async login(user: any) {
+    const payload = { userName: user.email, sub: user.id };
+    const token = this.jwtService.sign(payload);
+    this.tokenService.save(token, user.email);
+    return {
+      access_token: token,
     };
   }
 }
---
+++ graf-wall-api/src/token/dto/refresh.token.dto.ts
@@ -0,0 +1,5 @@
+export interface RefreshTokenDto {
+  oldToken: string;
+}
+
+export {};
