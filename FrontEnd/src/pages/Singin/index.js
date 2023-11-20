bc00dd5d73f7b424f1a066302e9e9951ba742fad
Rodrigo schiavone <rodrigoschiavonero@gmail.com>
2023-11-20 15:09:07 -0300

    validando a existencia do email no banco antes de efetuar o cadastro e uma caixa de diálogo para avisar que o uruário foi cadastrado

--- FrontEnd/src/pages/Singin/index.js
+++ FrontEnd/src/pages/Singin/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Modal, TouchableOpacity } from "react-native";
 import { Button, Input, Text } from "@rneui/themed";
 import { Formik } from "formik";
 import validationSchema from "../../validations/userRegistrationValidationSchema";
 import usuarioService from "../../services/userService";
 
 export default function SignIn({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+
   const returnToLogin = () => {
     navigation.reset({
       index: 0,
@@ -25,104 +28,150 @@ export default function SignIn({ navigation }) {
       .cadastrar(formattedValues)
       .then((response) => {
         const titulo = response.data.status ? "Sucesso" : "Erro";
-
-        //Alert.alert(titulo, response.data.mensagem)
+        setModalMessage(response.data.message);
+        setModalVisible(true);
       })
       .catch((error) => {
-        //Alert.alert("Erro", "Houve um erro inesperado")
+        const titulo = response.data.status ? "Sucesso" : "Erro";
+        setModalMessage(response.data.message);
+        setModalVisible(true);
       });
-    console.log(formattedValues);
-    returnToLogin();
   };
 
   return (
-    <Formik
-      initialValues={{ nome: "", sobrenome: "", email: "", password: "" }}
-      validationSchema={validationSchema}
-      onSubmit={(values) => saveUser(values)}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        errors,
-        touched,
-      }) => (
-        <View>
-          <Text h1>Cadastre-se já!</Text>
+    <View>
+      <Formik
+        initialValues={{ nome: "", sobrenome: "", email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => saveUser(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            <Text h1>Cadastre-se já!</Text>
 
-          <Input
-            placeholder="Nome"
-            leftIcon={{
-              type: "font-awesome",
-              name: "user",
+            <Input
+              placeholder="Nome"
+              leftIcon={{
+                type: "font-awesome",
+                name: "user",
+              }}
+              onChangeText={handleChange("nome")}
+              onBlur={handleBlur("nome")}
+              value={values.nome}
+            />
+            {touched.nome && errors.nome && (
+              <Text style={{ color: "red" }}>{errors.nome}</Text>
+            )}
+
+            <Input
+              placeholder="Sobrenome"
+              leftIcon={{
+                type: "font-awesome",
+                name: "user",
+              }}
+              onChangeText={handleChange("sobrenome")}
+              onBlur={handleBlur("sobrenome")}
+              value={values.sobrenome}
+            />
+            {touched.sobrenome && errors.sobrenome && (
+              <Text style={{ color: "red" }}>{errors.sobrenome}</Text>
+            )}
+
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              leftIcon={{
+                type: "font-awesome",
+                name: "envelope",
+              }}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            {touched.email && errors.email && (
+              <Text style={{ color: "red" }}>{errors.email}</Text>
+            )}
+
+            <Input
+              placeholder="Senha"
+              leftIcon={{
+                type: "font-awesome",
+                name: "lock",
+              }}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              secureTextEntry={true}
+            />
+            {touched.password && errors.password && (
+              <Text style={{ color: "red" }}>{errors.password}</Text>
+            )}
+
+            <Button
+              title="Cadastrar"
+              buttonStyle={{ backgroundColor: "rgba(39, 39, 39, 1)" }}
+              onPress={handleSubmit}
+            />
+            <Button
+              title="voltar"
+              titleStyle={{ color: "rgba(1,1,1,1)" }}
+              onPress={returnToLogin}
+              buttonStyle={{ backgroundColor: "rgba(39, 39, 39, 0)" }}
+            />
+          </View>
+        )}
+      </Formik>
+
+      {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 10,
+              alignItems: "center",
             }}
-            onChangeText={handleChange("nome")}
-            onBlur={handleBlur("nome")}
-            value={values.nome}
-          />
-          {touched.nome && errors.nome && (
-            <Text style={{ color: "red" }}>{errors.nome}</Text>
-          )}
-
-          <Input
-            placeholder="Sobrenome"
-            leftIcon={{
-              type: "font-awesome",
-              name: "user",
-            }}
-            onChangeText={handleChange("sobrenome")}
-            onBlur={handleBlur("sobrenome")}
-            value={values.sobrenome}
-          />
-          {touched.sobrenome && errors.sobrenome && (
-            <Text style={{ color: "red" }}>{errors.sobrenome}</Text>
-          )}
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            leftIcon={{
-              type: "font-awesome",
-              name: "envelope",
-            }}
-            onChangeText={handleChange("email")}
-            onBlur={handleBlur("email")}
-            value={values.email}
-          />
-          {touched.email && errors.email && (
-            <Text style={{ color: "red" }}>{errors.email}</Text>
-          )}
-
-          <Input
-            placeholder="Senha"
-            leftIcon={{
-              type: "font-awesome",
-              name: "lock",
-            }}
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-            secureTextEntry={true}
-          />
-          {touched.password && errors.password && (
-            <Text style={{ color: "red" }}>{errors.password}</Text>
-          )}
-
-          <Button
-            title="Cadastrar"
-            buttonStyle={{ backgroundColor: "rgba(39, 39, 39, 1)" }}
-            onPress={handleSubmit}
-          />
-          <Button
-            title="voltar"
-            titleStyle={{ color: "rgba(1,1,1,1)" }}
-            onPress={returnToLogin}
-            buttonStyle={{ backgroundColor: "rgba(39, 39, 39, 0)" }}
-          />
+          >
+            <Text>{modalMessage}</Text>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                returnToLogin();
+              }}
+              style={{
+                marginTop: 10,
+                padding: 10,
+                backgroundColor: "rgba(39, 39, 39, 1)" ,
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ color: "white" }}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      )}
-    </Formik>
+      </Modal>
+    </View>
   );
 }
